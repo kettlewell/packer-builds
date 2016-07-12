991ef95a2e8ef788085bf80a87f5cb1c842d8b6d
Matthew Kettlewell <matt@kettlewell.net>
2016-07-12 07:00:14 -0500

    commented out a buggy line in cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -10,19 +10,22 @@ systemctl stop crond
 #service rsyslog stop
 #service crond stop
 
-rm -f /etc/ssh/ssh_host_*
 
 find /var/log/ -type f | while read file; do cat /dev/null > "${file}";done
 
-rm -f /tmp/*
+# rm -f /tmp/*
 
 find /home/ -mindepth 1 -maxdepth 1 -type d -exec rm -fr {} \;
 find /root/ -mindepth 1 -maxdepth 1 -type d -exec rm -fr {} \;
+
 find /root/ -type f -exec rm -f {} \;
+
 history -c
 
 shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 shred -u /root/.*history  /home/centos/.*history /home/*/.*history
+rm -f /etc/ssh/ssh_host_*
+
 dd if=/dev/zero of=/zeros bs=1M
 rm -f /zeros
 
