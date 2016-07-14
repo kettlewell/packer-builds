90a8c7f898734daed86042a17025aa1aa24a7904
Matthew J Kettlewell <matt@kettlewell.net>
2016-07-14 07:05:56 -0500

    Updated pip_install.sh to be more verbose, and add some debugging while investigating why pip didn't install jinja2

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash 
 set -eux
 
+echo "Pip List BEFORE Install"
+pip list
+
 echo "Start PIP Install"
-pip install paramiko PyYAML Jinja2 httplib2 six
+pip -vvv --no-cache-dir --disabe-pip-version-check --log /var/log/pip_install.log install paramiko PyYAML Jinja2 httplib2 six
+
+echo "Pip List AFTER Install"
+pip list
+
