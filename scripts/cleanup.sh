41e75e71e588558eb45e1cf629679ad6fd5bd3ac
Matthew J Kettlewell <matt@kettlewell.net>
2016-07-15 07:05:58 -0500

    removed shredding and tmp removal ... and added notes to use amazon-ebs packer builds in the future

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -21,18 +21,6 @@ find /root/ -type f -exec rm -f {} \;
 
 history -c
 
-echo "shredding..."
-echo "SHREDDING ON HOLD WHILE INVESTIGATING WHY IT CRASHES THINGS.... "
-echo "Suspected of a bash glob issue... "
-# shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
-
-# shred -u /root/.*history  /home/centos/.*history /home/*/.*history
-
-echo "rm  /tmp/ "
-rm -rf /tmp/*
-echo "rm /etc/ssh/ssh_host_"
-rm -f /etc/ssh/ssh_host_*
-
 echo "zeroing out the bits..."
 dd if=/dev/zero of=/zeros bs=1M || echo "dd exit code $? is suppressed"
 rm -f /zeros
